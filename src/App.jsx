cbfa6772a0a733d404b5a115e87b860ea4e91e73
jiyoo <deepdive.jiyoo@gmail.com>
2025-08-11 15:57:59 +0900

    연결 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>방가방가</h1>
+      <h1>방가방가 하이</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
