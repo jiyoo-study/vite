578cfca7f8a094254ceb5653f763c5cd7fd991e8
jiyoo <deepdive.jiyoo@gmail.com>
2025-08-11 15:51:22 +0900

    feat: Add GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/jiyoo-study/vite', // 여기에 실제 저장소 이름 입력
-  build: {
-    outDir: 'dist'
-  }
+  // 깃허브 페이지스 배포를 위한 기본 경로 설정
+  base: '/vite/',
 })
